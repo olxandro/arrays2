cfdf0cf7f7416d2f521e2f9109ba5c7a81817af4
Olesia <olesia@.ru>
2022-08-18 19:01:47 +0300

    сдача дз

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,95 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] arr = generateRandomArray();
+        //System.out.println(Arrays.toString(arr));
+        //task_1
+        /*
+        Первым делом бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        в формате: «Сумма трат за месяц составила … рублей».
+         */
+
+        int sum = 0;
+        for (int j : arr) {
+            sum += j;
+
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей.");
+
+        //task_2
+        /*
+        Следующая задача — найти минимальную и максимальную трату за день.
+        Нужно написать программу, которая решит эту задачу, и вывести в консоль результат
+        в формате: «Минимальная сумма трат за день составила … рублей. Максимальная сумма трат за день составила …
+        рублей».
+         */
+        int min = arr[0];
+        int max = 0;
+        for (int j : arr) {
+            if (j < min) {
+                min = j;
+            }
+            if (j > max) {
+                max = j;
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей.");
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей.");
+
+        //task_3
+        /*
+        А теперь нам нужно понять, какую в среднем сумму наша компания тратила в течение данных 30 дней.
+        Нужно написать программу, которая посчитает среднее значение трат за месяц (то есть сумму всех трат за месяц
+        поделить на количество дней), и вывести в консоль результат в формате: «Средняя сумма трат за месяц составила
+        … рублей».
+         */
+
+        double average = 0;
+//        sum = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            sum = sum + arr[i];
+//        }
+        average = (double)sum / arr.length;
+        System.out.println(arr.length);
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+
+        //task_4
+        /*
+        Отойдем от подсчетов.
+        В нашей бухгалтерской книге появился баг. Что-то пошло нет так, и Ф.И.О. сотрудников начали отображаться в
+        обратную сторону. Т. е. вместо «Иванов Иван Иванович» мы имеем «чивонавИ навИ вонавИ».
+        Данные с именами сотрудников хранятся в виде массива символов (char[]).
+        Напишите код, который в случае такого бага будет выводить Ф. И. О. сотрудников вкорректном виде. В качестве
+        данных для массива используйте:
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        В результате в консоль должно быть выведено "Ivanov Ivan".
+        **Важно**: не используйте дополнительные массивы для решения этой задачи. Необходимо корректно пройти по
+        массиву циклом и распечатать его элементы в правильном порядке.
+         */
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = 0; i < reverseFullName.length/2; i++) {
+            char temp = reverseFullName[i];
+            reverseFullName[i] = reverseFullName[reverseFullName.length - i -1];
+            reverseFullName[reverseFullName.length- i - 1] = temp;
+        }
+        System.out.print("Наш массив - ");
+        System.out.println(Arrays.toString(reverseFullName));
     }
+
+
+
+    public static int[] generateRandomArray() {
+        Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
 }
